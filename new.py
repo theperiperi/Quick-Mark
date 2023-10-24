7a076f20432c4d71efd12a9a6a0baf3d03b68719
priya sridhar <121922820+theperiperi@users.noreply.github.com>
2023-10-24 15:57:02 +0530

    Delete new.py

--- new.py
+++
@@ -1,8 +0,0 @@
-#it is sufficient to run line 2 and 3 only once 
-from pickling_images import pickle
-pickle.imagesample()
-
-from main import attendance
-attendance.mark_attendance()
-from showattendancedb import db_show
-db_show.view_db()
